f0dd4dbe5a4156f4fe3c642e4abfb9f96017df26
trpsimoes <trpsimoes@gmail.com>
2026-04-29 17:39:04 +0100

    Select custom arrow; ANIMATIONS toggle in main.js

--- placeholder/assets/js/main.js
+++ placeholder/assets/js/main.js
@@ -3,9 +3,17 @@ import { initLogo }       from './logo.js';
 import { initAnimations } from './animations.js';
 import './modal.js';
 
-const { lucide } = window;
+const ANIMATIONS = true;
+
+const { gsap, lucide } = window;
 
 lucide.createIcons();
 initParticles('bg-canvas');
 initLogo();
-initAnimations();  // comment out to disable entrance animations while working on layout
+
+if (ANIMATIONS) {
+    initAnimations();
+} else {
+    gsap.set(['.pl-logo', '.contact', '.cta-wrap', '.socials', '.footer'], { opacity: 1 });
+    gsap.set('#divider', { width: 80 });
+}
